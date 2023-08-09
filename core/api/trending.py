a9a6f50c3b9eeecbafb3e2c9f92955edf08c1451
wilberh <wilberhdez@yahoo.com>
2023-08-09 00:11:08 -0400

    cleaning code

--- core/api/trending.py
+++ core/api/trending.py
@@ -1,16 +1,16 @@
-import pandas as pd
-import numpy as np
-
-from pytrends.request import TrendReq
-from google.cloud import bigquery
-
-import yfinance as yf
-
 import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 from newsapi import NewsApiClient
+
+from pytrends.request import TrendReq
+from google.cloud import bigquery
+
+import pandas as pd
+import numpy as np
+
+import yfinance as yf
 
 from datetime import date
 import sys, traceback
@@ -33,9 +33,11 @@ def get_stock_price(stock, strt_dt, end_dt):
         end_dt: str
             Download end date string (YYYY-MM-DD) or _datetime, exclusive.
             Default is now
-    'closing price' or 'adjusted closing price' is used because - gives you the most info you need to focus for a day 
+    'closing price' or 'adjusted closing price' is used because - gives you
+    the most info you need to focus for a day.
     DONNOT use the following:
-    *opening price is not reliable (subject to manipulation because of timelag between Asian and US markets; so there will be a huge difference)
+    *opening price is not reliable (subject to manipulation because of
+    timelag between Asian and US markets; so there will be a huge difference)
     *low and high price doesn't give you the big picture
     # stock = ['^GSPC','AAPL','MSFT','INTC']
     """
@@ -59,7 +61,7 @@ def trending_by_country(company, country, strt_dt, end_dt):
     :Parameters:
         company : str
             The company name.
-            E.g. "apple" or "microsoft" 
+            E.g. "apple" or "microsoft"
         country: str
             The country code to search for news articles data
             E.g. "us" or "uk"
@@ -76,7 +78,8 @@ def trending_by_country(company, country, strt_dt, end_dt):
     expressed in social media. It is fully open-sourced.
 
     NLTK provides us with a VADER implementation for easy use -
-    Sentiment analyzer expects a full sentence as input and returns scores along thes dimensions:
+    Sentiment analyzer expects a full sentence as input and returns scores
+    along thes dimensions:
     'compound' value gives you the average score for the text
     ranges from -1 to 1
     {'neg': 0.0, 'neu': 0.0, 'pos': 1.0, 'compound': 0.4404}
@@ -92,7 +95,8 @@ def trending_by_country(company, country, strt_dt, end_dt):
         sources_country = sources_df[(sources_df['language'] == language) 
                                     & (sources_df['category'] == 'business')
                                     & (sources_df['country'] == country)]
-        max_sources_api = ','.join(list(sources_country['id'].iloc[:20]))  # 20 max sources allowed by api
+        # 20 max sources allowed by api
+        max_sources_api = ','.join(list(sources_country['id'].iloc[:20]))
 
         page = 1
         params = {'q': company, 
@@ -111,7 +115,8 @@ def trending_by_country(company, country, strt_dt, end_dt):
 
         while total_results > total_per_pages:
             params['page']: page
-            resp = requests.get('https://newsapi.org/v2/everything', params=params, headers=headers)
+            resp = requests.get('https://newsapi.org/v2/everything',
+                                params=params, headers=headers)
             resp_all_articles = resp.json()
             all_articles.extend(resp_all_articles['articles'])
             total_results = resp_all_articles['totalResults']
@@ -124,17 +129,20 @@ def trending_by_country(company, country, strt_dt, end_dt):
         titles_sentiment = pd.DataFrame([], columns=columns)
         sentiment = SentimentIntensityAnalyzer()
 
-        for date, source, title in zip(news_articles['publishedAt'], news_articles['source'], news_articles['title']):
+        for date, source, title in zip(news_articles['publishedAt'],
+                                       news_articles['source'],
+                                       news_articles['title']):
             score = sentiment.polarity_scores(title)
             new_row = pd.Series({'date': date[:10].replace('-', ''),
                                  'source': source['name'],
                                  'title': title,
                                  'compound': score['compound']})
-            titles_sentiment = pd.concat([titles_sentiment, new_row.to_frame().T], ignore_index=True)
+            titles_sentiment = pd.concat([titles_sentiment,
+                                          new_row.to_frame().T],
+                                          ignore_index=True)
 
         titles_sentiment.drop_duplicates(subset=['date', 'source', 'title'])
         return titles_sentiment.to_dict('records')
     except Exception as ex:
         formatted_lines = traceback.format_exc().splitlines()
         return {"error": formatted_lines[0]+' \n'+formatted_lines[-1]}
-
